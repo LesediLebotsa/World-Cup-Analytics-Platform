c9b6970de3031ff30fb0ee5460573f4bd6772b70
Lesedi <lede.lebotsa1598@gmail.com>
2026-07-10 19:58:36 +0200

    Add API root and health endpoints

--- app/main.py
+++ app/main.py
@@ -1,71 +1,53 @@
-from api_services.middleware.logging import LoggingMiddleware
-from api_services.middleware.timing import TimingMiddleware
 from fastapi import FastAPI
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
+from app.exceptions import rate_limit_handler
 from app.routers import (
     analytics,
     history,
     prediction,
     teams
 )
-from slowapi.errors import RateLimitExceeded
-from slowapi.middleware import SlowAPIMiddleware
-from app.exceptions import rate_limit_handler
+from api_services.middleware.logging import LoggingMiddleware
+from api_services.middleware.timing import TimingMiddleware
 from api_services.security.rate_limit import limiter
 
 app = FastAPI(
     title="World Cup Analytics & Prediction Platform",
 )
 
+# Middleware
+app.add_middleware(LoggingMiddleware)
+app.add_middleware(TimingMiddleware)
+app.add_middleware(SlowAPIMiddleware)
+
+app.state.limiter = limiter
+
+app.add_exception_handler(
+    RateLimitExceeded,
+    rate_limit_handler
+)
+
+# Routers
 app.include_router(teams.router)
 app.include_router(analytics.router)
 app.include_router(prediction.router)
 app.include_router(history.router)
 
-app.add_middleware(LoggingMiddleware)
-app.add_middleware(TimingMiddleware)
-app.state.limiter = limiter
-
-app.add_exception_handler(
-
-    RateLimitExceeded,
-
-    rate_limit_handler
-
-)
-app.add_middleware(SlowAPIMiddleware)
-
-# for a landing page
+# Root endpoint
 @app.get("/")
 def root():
-
     return {
-
-        "service": "World Cup Analytics API",
-
+        "application": "World Cup Analytics & Prediction Platform",
         "status": "online",
-
+        "version": "1.1.0",
         "documentation": "/docs"
-
     }
+
+# Health endpoint
 @app.get("/health")
 def health():
-
     return {
         "status": "healthy"
-    }
-
-@app.get("/")
-
-def root():
-
-    return {
-
-        "application": "World Cup Analytics API",
-
-        "status": "online",
-
-        "version": "1.1.0",
-
-        "documentation": "/docs"
-
     }
